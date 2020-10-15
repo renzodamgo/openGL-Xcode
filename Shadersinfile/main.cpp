afbbe3a38f9acb8b55ae3b0ab2ba08c634f5ec0d
Renzo Damian <renzo.damian.go@gmail.com>
2020-10-15 18:53:08 -0500

    Initial Commit

---
+++ Shadersinfile/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Shadersinfile
+//
+//  Created by renzo damian on 15/10/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
